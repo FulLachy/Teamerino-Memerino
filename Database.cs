083089b55d728a818378bb771106a2f19caebb41
xyli123 <xyli2468@gmail.com>
2017-04-09 22:01:42 +1000

    Add files via upload
    incomplete item.

---
+++ Database.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamerino_Memerino
+{
+    class Database
+    {
+        private List<InventoryItem> itemlist;
+
+        private List<SalesRecord> recordlist;
+
+        //Load item and record
+        public Database()
+        {
+
+        }
+
+        public List<InventoryItem> ShowItem()
+        {
+
+        }
+
+        public List<SalesRecord> ShowRecord()
+        {
+
+        }
+
+        public void AddItem()
+        {
+
+        }
+
+        public void DeletItem()
+        {
+
+        }
+
+        public void EditItem()
+        {
+
+        }
+
+        public void AddRecord()
+        {
+
+        }
+
+        public void DeleteRecord()
+        {
+
+        }
+
+        public void EditRecord()
+        {
+
+        }
+
+        public void WriteToFile()
+        {
+
+        }
+
+
+
+    }
+}
